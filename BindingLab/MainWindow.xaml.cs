6179bb1c5447190f29c30f0a45879897667125fc
Maikel <Maikelsitbon@gmail.com>
2018-11-18 01:12:44 +0200

    Add project files.

---
+++ BindingLab/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingLab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> Persons { get; set; }
+        public VisibilityConverter converter { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            converter = new VisibilityConverter();
+            Persons = new ObservableCollection<Person>();
+            AddRandomPerson();
+            DataContext = this;
+
+
+        }
+
+        private void AddRandomPerson()
+        {
+            Persons.Add(new Person(new Random().Next(10, 80), "RandomPerson"));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddRandomPerson();
+        }
+    }
+}
---
+++ BindingLab/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingLab
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int _age, string _name)
+        {
+            Age = _age;
+            Name = _name;
+        }
+    }
+}
